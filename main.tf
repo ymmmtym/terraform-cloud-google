03adec7b9309962d0aec05447e8df41650a79cf8
ymmmtym <26668352+ymmmtym@users.noreply.github.com>
2026-07-19 11:26:27 +0900

    chore: add google-beta to required_providers

--- main.tf
+++ main.tf
@@ -14,5 +14,9 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 7.0"
     }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 7.0"
+    }
   }
 }
